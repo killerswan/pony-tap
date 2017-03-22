350fa66bd5f0cd7c1e0a65dcfba1a2a18403d1fa
Kevin Cantú <kevincantu@gmail.com>
2017-03-21 18:39:47 -0700

    Add the ponyc formula from homebrew-core@a34bd21f

---
+++ Formula/ponyc.rb
@@ -0,0 +1,47 @@
+class Ponyc < Formula
+  desc "Object-oriented, actor-model, capabilities-secure programming language"
+  homepage "http://www.ponylang.org"
+  url "https://github.com/ponylang/ponyc/archive/0.11.3.tar.gz"
+  sha256 "0b88009c636669192baee71589cd37885fe0f39bd24008ccc20780d925c18a2c"
+  revision 1
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9fce90dedcef838ba065caf2962b9982e193dc0f021ecac15a0a4b4a599b8680" => :sierra
+    sha256 "2ae598e71c41a822d12892cb3c73ffa944b7dcc151a73efe6f4a06c8c212b0fa" => :el_capitan
+    sha256 "418bae1246b895ded8c57fc0e3139cc29ee4c74d6ae575c2b808dc7d5b321286" => :yosemite
+  end
+
+  depends_on :macos => :yosemite
+  depends_on "llvm@3.9"
+  depends_on "libressl"
+  depends_on "pcre2"
+  needs :cxx11
+
+  # https://github.com/ponylang/ponyc/issues/1274
+  # https://github.com/Homebrew/homebrew-core/issues/5346
+  pour_bottle? do
+    reason <<-EOS.undent
+      The bottle requires Xcode/CLT 8.0 or later to work properly.
+    EOS
+    satisfy { DevelopmentTools.clang_build_version >= 800 }
+  end
+
+  def install
+    ENV.cxx11
+    ENV["LLVM_CONFIG"] = "#{Formula["llvm@3.9"].opt_bin}/llvm-config"
+    system "make", "config=release", "destdir=#{prefix}", "install", "verbose=1"
+  end
+
+  test do
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/stdlib"
+
+    (testpath/"test/main.pony").write <<-EOS.undent
+    actor Main
+      new create(env: Env) =>
+        env.out.print("Hello World!")
+    EOS
+    system "#{bin}/ponyc", "test"
+    assert_equal "Hello World!", shell_output("./test1").strip
+  end
+end
